cac840bcde20aa2b794ff705eacde34158978627
k158124456 <cska20015@g.nihon-u.ac.jp>
2023-07-20 23:34:11 +0900

    モジュール分別化

--- umalib/__init__.py
+++ umalib/__init__.py
@@ -1,3 +1,4 @@
-from umalib.logger import *
-from umalib.database import *
-from umalib.selenium import *
+# from umalib.logger import getlogger じゃなく、from umalib import getlogger とかで使えるようにできるけど、ファイル単位で分けたほうがいいと思ったのでコメントアウト
+# from umalib.logger import *
+# from umalib.database import *
+# from umalib.selenium import *
